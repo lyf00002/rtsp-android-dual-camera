e5ba1b9931c30a4545817fdf836775cad8197fb8
leiyh <290726098@qq.com>
2019-12-30 18:59:09 +0800

    request permission

--- app/src/main/java/net/majorkernelpanic/example1/MainActivity.java
+++ app/src/main/java/net/majorkernelpanic/example1/MainActivity.java
@@ -3,12 +3,19 @@ package net.majorkernelpanic.example1;
 import net.majorkernelpanic.streaming.SessionBuilder;
 import net.majorkernelpanic.streaming.gl.SurfaceView;
 import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.WindowManager;
 
 /**
@@ -20,6 +27,16 @@ public class MainActivity extends Activity {
 
 	private SurfaceView mSurfaceView;
 
+	private String[] permissions = { Manifest.permission.INTERNET,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			Manifest.permission.RECORD_AUDIO,
+			Manifest.permission.CAMERA,
+			Manifest.permission.WAKE_LOCK
+	};
+
+	private void startRequestPermission() {
+		ActivityCompat.requestPermissions(this, permissions, 321);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,7 +45,19 @@ public class MainActivity extends Activity {
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 		mSurfaceView = (SurfaceView) findViewById(R.id.surface);
-		
+
+
+		for (int j = 0; j < permissions.length; j++) {
+			// 检查该权限是否已经获取
+			int i = ContextCompat.checkSelfPermission(getApplicationContext(), permissions[j]);
+			// 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+			if (i != PackageManager.PERMISSION_GRANTED) {
+				Log.d(TAG, "onCreate:  request extern storge");
+				startRequestPermission();
+			}
+		}
+
+
 		// Sets the port of the RTSP server to 1234
 		Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
 		editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
@@ -41,10 +70,10 @@ public class MainActivity extends Activity {
 		.setContext(getApplicationContext())
 		.setAudioEncoder(SessionBuilder.AUDIO_NONE)
 		.setVideoEncoder(SessionBuilder.VIDEO_H264);
-		
+
 		// Starts the RTSP server
 		this.startService(new Intent(this,RtspServer.class));
 
 	}
-	
+
 }
